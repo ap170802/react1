238c00345913b6ad5d703ea6b6a4b9f1e534fd55
ap170802 <ankitpandey4140@gmail.com>
2024-04-03 21:36:20 +0530

    lecture1-4

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+const heading = document.createElement('h1');
+        heading.innerHTML="hello in the world of js"
+        const root= document.getElementById('root');
+        root.appendChild(heading);
